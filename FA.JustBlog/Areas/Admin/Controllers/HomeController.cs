dc10cff2bfd2ee6b6c4348a189fdd39223dac46d
Na30vn <nhatanhsuper02@gmail.com>
2024-05-17 10:53:31 +0700

    init project

---
+++ FA.JustBlog/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FA.JustBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/Home")]
+    // [Authorize(Roles = "Blog_Owner,CONTRIBUTOR")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
